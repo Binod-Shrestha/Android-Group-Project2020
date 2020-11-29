6a4af37f9790ebd7cd51b14d1edf54e26d478e3c
Hemraj Kafle <kafleh@sheridancollege.ca>
2020-11-29 13:57:34 -0500

    Created ExpenseEntity, ExpenseDao and ExpensesDatabase

---
+++ app/src/main/java/org/sheridancollege/expensetracker/db/ExpensesDatabase.kt
@@ -0,0 +1,28 @@
+package org.sheridancollege.expensetracker.db
+
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import android.content.Context
+
+@Database(entities = [ExpenseEntity::class], version = 1)
+abstract class ExpensesDatabase : RoomDatabase() {
+    abstract fun expenseDao():ExpenseDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: ExpensesDatabase? = null
+
+        fun getInstance(context: Context): ExpensesDatabase {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                    context,
+                    ExpensesDatabase::class.java,
+                    "expenses.db")
+                    .build()
+            }
+            return INSTANCE as ExpensesDatabase
+        }
+    }
+}
