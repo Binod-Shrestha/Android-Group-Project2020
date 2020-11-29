219f880aee431015a7bbb085b3edd9afce6fc452
Binod Shrestha <shresthabinod.softdev@gmail.com>
2020-11-28 21:23:14 -0500

    refactored the UIs and navigation

--- app/src/main/java/org/sheridancollege/expensetracker/ui/income/EarningsFragment.kt
+++ app/src/main/java/org/sheridancollege/expensetracker/ui/income/EarningsFragment.kt
@@ -1,23 +1,19 @@
-package org.sheridancollege.expensetracker
+package org.sheridancollege.expensetracker.ui.income
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.sheridancollege.expensetracker.R
 
 
-class SummaryFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+class EarningsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_summary, container, false)
+        return inflater.inflate(R.layout.fragment_earnings, container, false)
     }
 
 
