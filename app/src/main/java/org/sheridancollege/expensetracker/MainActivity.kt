12fdfb2ff5fc3f30c6361513181db8e74650afad
Binod Shrestha <shresthabinod.softdev@gmail.com>
2020-12-01 16:54:01 -0500

    code clean up

--- app/src/main/java/org/sheridancollege/expensetracker/MainActivity.kt
+++ app/src/main/java/org/sheridancollege/expensetracker/MainActivity.kt
@@ -3,13 +3,10 @@ package org.sheridancollege.expensetracker
 import android.os.Bundle
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.ui.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
