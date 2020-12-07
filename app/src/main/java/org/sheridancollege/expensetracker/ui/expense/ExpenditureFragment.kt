5ebfad9b5e762e6e4f72e4ac399fe5baf84db76f
Hemraj Kafle <kafleh@sheridancollege.ca>
2020-12-06 21:35:33 -0500

    Added navigations from/to ExpenditureFragment

--- app/src/main/java/org/sheridancollege/expensetracker/ui/expense/ExpenditureFragment.kt
+++ app/src/main/java/org/sheridancollege/expensetracker/ui/expense/ExpenditureFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.expense_item.*
+import kotlinx.android.synthetic.main.expense_item.view.*
 import kotlinx.android.synthetic.main.fragment_expenditure.*
 import org.sheridancollege.expensetracker.R
 import org.sheridancollege.expensetracker.databinding.FragmentExpenditureBinding
@@ -25,16 +28,14 @@ class ExpenditureFragment : Fragment() {
             view.findNavController().navigate(R.id.action_expenditureFragment_to_addExpense)
         }
 
-        /*var expenseList = generateExpensesList()
-
-        recycleView.adapter = MyRecyclerView(expenseList)
-        recycleView.layoutManager = LinearLayoutManager(this.context)
-        recycleView.setHasFixedSize(true)*/
         setHasOptionsMenu(true)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+
         var expenseList = generateExpensesList()
 
 
